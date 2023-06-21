f7e04783ddd48ad527a07333e7c225f81599e944
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-20 23:43:49 -0300

    created the screens fo app

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,68 @@
-import { Center, Text} from 'native-base'
+import { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { HStack, VStack, FlatList, Heading, Text} from 'native-base'
+
+import { Group } from '@components/Group'
+import { HomeHeader } from '@components/HomeHeader'
+import { ExerciseCard } from '@components/ExerciseCard'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 export function Home(){
+  const [groups,setGroups] = useState(['Costas', 'Biceps', 'Triceps', 'Ombro'])
+  const [exercises,setExercises] = useState(['Puxada frontal', 'Remada curva', 'Remada unilateral', 'Levantamento terra'])
+  const [groupSelected,setGroupSelected] = useState('costas')
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails(){
+    navigation.navigate('exercise')
+  }
+
   return(
-    <Center flex={1}>
-      <Text color="white">
-        Home
-      </Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader/>
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Group
+            name={item}
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <ExerciseCard
+            onPress={handleOpenExerciseDetails}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{paddingBottom: 20}}
+        />  
+        
+      </VStack>
+    </VStack>
   )
 }
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,87 @@
-import { Center, Text} from 'native-base'
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading} from 'native-base'
+
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
+
+const PHOTO_SIZE = 33;
 
 export function Profile(){
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return(
-    <Center flex={1}>
-      <Text color="white">
-        Profile
-      </Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil"/>
+
+      <ScrollView contentContainerStyle={{paddingBottom: 36}}>
+        <Center mt={6} px={10}>
+          {
+            photoIsLoading ?
+              <Skeleton 
+                w={PHOTO_SIZE} 
+                h={PHOTO_SIZE} 
+                rounded="full" 
+                startColor="gray.500"
+                endColor="gray.400"
+    
+              />
+            :
+              <UserPhoto 
+                source={{uri:'https://github.com/Tiotedd.png'}}
+                alt="Foto do usuário"
+                size={PHOTO_SIZE}
+              />
+          }
+
+          <TouchableOpacity>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input
+            bg="gray.600"
+            placeholder="Nome"
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="Email"
+            value="andreluisx2009@yahoo.com.br"
+            isDisabled
+          />
+
+          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
+            Alterar senha
+          </Heading>
+
+          <Input
+            bg="gray.600"
+            placeholder="Senha antiga"
+            secureTextEntry
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="Senha nova"
+            secureTextEntry
+          />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button
+            title="Atualizar"
+            mt={4}
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
