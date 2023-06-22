608cb7d73e9b898975ff02cfffc830c9755abdb5
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-22 00:43:24 -0300

    added function to select image from device

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading} from 'native-base'
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker'
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast} from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
@@ -11,6 +13,46 @@ const PHOTO_SIZE = 33;
 
 export function Profile(){
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto,setUserPhoto] = useState('https://github.com/Tiotedd.png')
+
+  const toast = useToast()
+
+  async function handleUserPhotoSelect(){
+    setPhotoIsLoading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4,4],
+        allowsEditing:true,
+        selectionLimit:1,
+      })
+  
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if(photoSelected.assets[0].uri){
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+        
+        if(photoInfo.exists && (photoInfo.size/1024/1024 ) > 5){
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+      
+      
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setPhotoIsLoading(false)
+    }
+  }
 
   return(
     <VStack flex={1}>
@@ -30,13 +72,13 @@ export function Profile(){
               />
             :
               <UserPhoto 
-                source={{uri:'https://github.com/Tiotedd.png'}}
+                source={{uri: userPhoto}}
                 alt="Foto do usuário"
                 size={PHOTO_SIZE}
               />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar foto
             </Text>
