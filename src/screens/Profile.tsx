53c919515df6d626424d208a6ab0558410adc989
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-27 17:05:43 -0300

    upload image to back-end

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -91,7 +91,28 @@ export function Profile(){
           })
         }
 
-        setUserPhoto(photoSelected.assets[0].uri)
+        const fileExtension = photoSelected.assets[0].uri.split('.').pop()
+        
+        const photoFile = {
+          name: `${user.name}.${fileExtension}`.toLowerCase(),
+          uri: photoSelected.assets[0].uri,
+          type: `${photoSelected.assets[0].type}/${fileExtension}`
+        } as any
+        
+        const userPhotoUploadForm = new FormData()
+        userPhotoUploadForm.append('avatar', photoFile)
+
+        await api.patch('/users/avatar', userPhotoUploadForm, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        
+        toast.show({
+          title: 'Foto atualizada.',
+          placement: 'top',
+          bgColor: 'green.500'
+        })
       }
       
       
